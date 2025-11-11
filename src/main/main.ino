2171ae14ea83510e0ad2b3395674c7e38bef9392
Lucas Clark <lucasclark997@gmail.com>
2025-11-11 15:39:16 -0300

    Se agrega la verificación "rele on" para iniciar el muestreo y el envío de consumo nulo cuando esta desconectado

--- src/main/main.ino
+++ src/main/main.ino
@@ -557,7 +557,7 @@ void loop() {
           }
         }
       }
-      analogContinuousStart();
+      if (digitalRead(rele) ) analogContinuousStart();
     }
   }else{
     // 4) purgar peers inactivos
@@ -573,7 +573,10 @@ void loop() {
       digitalWrite(rele, HIGH);
     }
   } 
-
+  if (!digitalRead(rele) ){
+    consensus_msg_t msg_send = {0.0, myPriority};
+    esp_now_send(broadcastAddress, (uint8_t*)&msg_send, sizeof(msg_send));
+  } 
   if (millis() - lastDisplayUpdate >= displayUpdateInterval) {
     lastDisplayUpdate = millis();
     displayUpdate(); 
